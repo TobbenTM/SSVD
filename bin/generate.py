de942bf1374c44bd945896061a32b4a3d4f47399
Tobias <m@dsen.tv>
2014-05-02 20:41:26 +0200

    May just about work now.
    Still needs to be tested.
    
    And implement cron setup in setup.py.

--- bin/generate.py
+++ bin/generate.py
@@ -14,22 +14,34 @@ def generate():
 	#datasource for database access
 	datasource = db.db("ssvd.db")
 	datasource.open()
+
+	#file handlers
+	index = open(settings["wwwpath"]+"/index.html", "w")
+
+	#writing template, should be done in a slicker way really
+	index.write("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>SSVD</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/ssvd.css" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<div class="site-wrapper-inner container">
+			<div class="masthead clearfix">
+				<h3 class="masthead-brand">SSVD</h3>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h1>Library:</h1>""")
 	
-	#looping through folders specified in settings
-	for folder in settings["folders"]:
+	# Library
 	
-		recent = datasource.getrecent(folder.lower())
-		
-		print("<h3>{0}:</h3>".format(folder))
-		
-		#Generate recent list for current folder
-		for row in recent:
-			time = datetime.datetime.fromtimestamp(row[2]).strftime('%d/%m-%Y  %H:%M')
-			print("<p>{3} - <a href=\"/video.html?video={0}{1}/{2}\">{2}</a></p>".format(folder, row[0], row[1], time))
-			print("<p>({0})</p>".format(row[0]))
-	
-	
-	print("<div class=\"panel-group\" id=\"MAIN\">");
+	index.write("<div class=\"panel-group\" id=\"MAIN\">");
 	
 	#looping through folders specified in settings
 	for folder in settings["folders"]:
@@ -37,7 +49,7 @@ def generate():
 		all = datasource.getall(folder.lower())
 		lastpath = ''
 		
-		print("""
+		index.write("""
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<h4 class="panel-title panel-head">
@@ -55,9 +67,9 @@ def generate():
 		for row in all:
 			if row[0] not in lastpath:
 				if lastpath is not '':
-					print("</div></div></div>")
+					index.write("</div></div></div>")
 						
-				print("""
+				index.write("""
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<h4 class="panel-title">
@@ -71,16 +83,40 @@ def generate():
 			<a href="/video.html?video={0}{2}/{3}">{3}</a></br>
 				""".format(folder, row[0].translate(None, ' !();,./-\''), row[0], row[1]))
 			else:
-				print("""<a href="/video.html?video={0}{1}/{2}">{2}</a></br>""".format(folder, row[0], row[1]))
+				index.write("""<a href="/video.html?video={0}{1}/{2}">{2}</a></br>""".format(folder, row[0], row[1]))
 			
 			lastpath = row[0]
 		
-		print("</div></div></div></div></div></div>")
+		index.write("</div></div></div></div></div></div>")
 			
-	print("</div></div></div>")
+	index.write("</div></div></div>")
+	index.write("""</div>
+<div class="col-md-6 recent">
+	<h1>Recently added:</h1>""")
+
+	# Recent
+
+	#looping through folders specified in settings
+	for folder in settings["folders"]:
 	
+		recent = datasource.getrecent(folder.lower())
+		
+		index.write("<h3>{0}:</h3>".format(folder))
+		
+		#Generate recent list for current folder
+		for row in recent:
+			time = datetime.datetime.fromtimestamp(row[2]).strftime('%d/%m-%Y  %H:%M')
+			index.write("<p>{3} - <a href=\"/video.html?video={0}{1}/{2}\">{2}</a></p>".format(folder, row[0], row[1], time))
+			index.write("<p>({0})</p>".format(row[0]))
+
+	index.write("""</div></div></div></div>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>""")
+	
+	#Close and commit database connection
 	datasource.close()
-	
-	
-	
-	
+
+	#Close file IO
+	index.close()
--- bin/setup.py
+++ bin/setup.py
@@ -2,10 +2,11 @@
 
 import db
 import json
+from distutils import dir_util
 
 def main():
 
-	# TODO: Copy required files to wwwpath
+	# TODO: Setup cron
 
 	#JSON object for settings
 	jsondata = open("conf.json")
@@ -25,7 +26,19 @@ def main():
 		print("Created table: "+str(folder.lower()))
 
 	print("Database setup completed")
-	
+
+
+	print("Copying files to web server...")
+
+	#copying necessary files to wwwpath
+	log = dir_util.copy_tree("../www/", settings["wwwpath"])
+	smbpath = open(settings["wwwpath"]+"/js/smbpath.js", "w")
+	smbpath.write(settings["sambapath"])
+	smbpath.close()
+
+	print("Completed copying files")
+
+
 	#Commit and close database when done
 	datasource.close()
 
